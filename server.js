e56256682eede5f26169d8b36f5e41bba99d5fc3
CycloSiphon <duckeak@gmail.com>
2023-03-12 12:11:16 +0530

    Semi Work Done on LiveNodeEnvironment

--- server.js
+++ server.js
@@ -1,59 +1,30 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const express = require("express")
-const port = process.env.PORT || 3000;
-const app = express();
+const path = require("path")
+
+const app = express()
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "public/src/css"))
+  )
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+app.use("/js", express.static(path.join(__dirname, "node_modules/popper.js/dist")))
+app.use("/js", express.static(path.join(__dirname, "public/src/js")))
 
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"))
+})
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(
-        __dirname,
-        "public",
-        req.url === "/" ? "index.html" : req.url
-    );
-    let filePathCSS = path.join(__dirname, 'node_modules/bootstrap/dist/js');
-    console.log(req.url);
-    let extName = path.extname(filePath);
-    let contentType = 'text/html';
-    app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-    app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-    app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-    switch (extName) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
-
-    console.log(`File path: ${filePath}`);
-    console.log(`Content-Type: ${contentType}`)
-
-    res.writeHead(200, { 'Content-Type': contentType });
-
-    const readStream = fs.createReadStream(filePath);
-    const readStreamCSS = fs.createReadStream(filePathCSS);
-    readStream.pipe(res);
-    readStreamCSS.pipe(res);
-});
-
-server.listen(port, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`)
-    } else {
-        console.log(`Server listening at port ${port}...`);
-    }
-});
-
+app.listen(5000, () => {
+  console.log("Listening on port " + 5000)
+})

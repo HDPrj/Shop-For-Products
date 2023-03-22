927b1dec26509268c490c5361ed9dc56b0541f9c
suswet <suswetpatekar9@gmail.com>
2023-03-22 11:06:13 +0530

    cart modal work

--- public/src/js/shop.js
+++ public/src/js/shop.js
@@ -19,17 +19,36 @@ shopWrapperDiv.classList.add('container', 'shop-wrapper');
 shopSection.appendChild(shopWrapperDiv);
 
 // Create the first row of products
+
+
 const firstRowDiv = document.createElement('div');
 firstRowDiv.classList.add('row');
 shopWrapperDiv.appendChild(firstRowDiv);
 
-const product1Div = createProductDiv('headphones_product1', 'Headphones1');
+const product1Div = createProductDiv('headphones_product1', 'Headphones1 price');
 const product2Div = createProductDiv('headphones_product2', 'Headphones2');
 const product3Div = createProductDiv('headphones_product3', 'Headphones3');
+
 
 firstRowDiv.appendChild(product1Div);
 firstRowDiv.appendChild(product2Div);
 firstRowDiv.appendChild(product3Div);
+
+// price tag addition
+
+
+// const newNode = document.createElement("span");
+// const parentDiv = document.getElementById("price").parentNode;
+// let sp2 = document.getElementById("price");
+// parentDiv.insertBefore(newNode, sp2);
+
+
+
+//  let sp1 = document.createElement("span");
+//  let sp2 = document.getElementById("price");
+//  let ProductDiv = sp2.product1Div;
+//  ProductDiv.insertBefore(sp1, sp2);
+
 
 // Create the second row of products
 const secondRowDiv = document.createElement('div');
@@ -55,15 +74,18 @@ function createProductDiv(imgAltText, productTitle) {
   productImg.classList.add('product-img');
   productDiv.appendChild(productImg);
 
-  const productTitleP = document.createElement('p');
-  productTitleP.textContent = productTitle;
+  const productTitleP = document.createElement('spam');
+  productTitleP.textContent = '1500';
+  productTitleP.classList.add("btn" ,"btn-outline-dark",'btn-cart');
   productDiv.appendChild(productTitleP);
 
   const addToCartBtn = document.createElement('a');
   addToCartBtn.setAttribute('href', '#');
   addToCartBtn.classList.add('btn', 'btn-secondary', 'btn-cart');
-  addToCartBtn.textContent = 'Add To Cart';
+  addToCartBtn.textContent = 'Add to cart' ;
   productDiv.appendChild(addToCartBtn);
+
+
 
   return productDiv;
 }

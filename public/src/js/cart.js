45bb960c6cf3832a74262fbc3f43000a2bd3f4dc
suswet <suswetpatekar9@gmail.com>
2023-04-01 12:08:18 +0530

    product json file done

--- public/src/js/cart.js
+++ public/src/js/cart.js
@@ -1,69 +1,18 @@
-const modalCart = document.createElement("div");
-modalCart.classList.add("modal", "fade");
-modalCart.setAttribute("id", "model-cart");
-modalCart.setAttribute("tabindex", "-1");
-modalCart.setAttribute("role", "dialog");
-modalCart.setAttribute("aria-labelledby", "exampleModalLabel");
-modalCart.setAttribute("aria-hidden", "true");
+// Create a sample product object
+const product = {
+    name: "headphone1",
+    price: "700",
+    image: "https://media.istockphoto.com/id/1325906677/photo/modern-design-of-black-color-wireless-earphone-isolated.jpg?b=1&s=170667a&w=0&k=20&c=NqkzH2XxDe81TB2WzNIHDIMr_r7NCeD8y-kDRmgCg0M=",
+    count: 0,
+    selected: "false"
+  };
+  
+  // Convert the product object to JSON format
+  const productJSON = JSON.stringify(product);
+  
+  // Log the JSON string to the console
+  console.log(typeof productJSON);
+  
 
-const modalDialog = document.createElement("div");
-modalDialog.classList.add("modal-dialog", "modal-lg");
 
-const modalContent = document.createElement("div");
-modalContent.classList.add("modal-content");
 
-const rootDiv = document.createElement("div");
-rootDiv.setAttribute("id", "root");
-modalContent.appendChild(rootDiv);
-
-const modalHeader = document.createElement("div");
-modalHeader.classList.add("modal-header");
-
-const modalTitle = document.createElement("h5");
-modalTitle.classList.add("modal-title");
-modalTitle.textContent = "My Cart";
-modalHeader.appendChild(modalTitle);
-
-const closeButton = document.createElement("button");
-closeButton.setAttribute("type", "button");
-closeButton.classList.add("close");
-closeButton.setAttribute("data-dismiss", "modal");
-closeButton.setAttribute("aria-label", "Close");
-modalHeader.appendChild(closeButton);
-
-modalContent.appendChild(modalHeader);
-
-const modalBody = document.createElement("div");
-modalBody.classList.add("modal-body");
-
-const cartItemDiv = document.createElement("div");
-cartItemDiv.setAttribute("id", "cart-item");
-cartItemDiv.textContent = "Your cart is empty";
-modalBody.appendChild(cartItemDiv);
-
-const totalPriceDiv = document.createElement("div");
-totalPriceDiv.innerHTML = `Total price: ₹<span class="total-cart"></span>`;
-modalBody.appendChild(totalPriceDiv);
-
-modalContent.appendChild(modalBody);
-
-const modalFooter = document.createElement("div");
-modalFooter.classList.add("modal-footer");
-
-const closeButtonFooter = document.createElement("button");
-closeButtonFooter.setAttribute("type", "button");
-closeButtonFooter.classList.add("btn", "btn-secondary");
-closeButtonFooter.setAttribute("data-dismiss", "modal");
-closeButtonFooter.textContent = "Close";
-modalFooter.appendChild(closeButtonFooter);
-
-const orderButton = document.createElement("button");
-orderButton.setAttribute("type", "button");
-orderButton.classList.add("btn", "btn-primary");
-orderButton.textContent = "Order now";
-modalFooter.appendChild(orderButton);
-
-modalContent.appendChild(modalFooter);
-
-modalDialog.appendChild(modalContent);
-modalCart.appendChild(modalDialog);

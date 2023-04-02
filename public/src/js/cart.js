fc64123302e22dc2dea3f0909cf6ea9ce4f0fb7b
suswet <suswetpatekar9@gmail.com>
2023-04-02 12:56:14 +0530

    payment modal

--- public/src/js/cart.js
+++ public/src/js/cart.js
@@ -1,6 +1,9 @@
 
 // const product =
 
+// const { default: axios } = require("axios");
+// const { response } = require("express");
+
 // `[
 //     {
 //         "name": "headphone1",
@@ -72,6 +75,25 @@
 
 
 
+    axios.get('./js/product.json')
+        .then(function (response) {
+
+            // handle success
+            
+            console.log(response.data.product);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+
+        // product.forEach(element => {
+
+           
+        // });
 
 
 

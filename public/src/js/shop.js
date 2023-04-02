13521a872eb6cc321faa3ffa15e4b0c095050922
CycloSiphon <duckeak@gmail.com>
2023-04-02 09:31:33 +0530

    Shop JS Update

--- public/src/js/shop.js
+++ public/src/js/shop.js
@@ -28,9 +28,9 @@ firstRowDiv.classList.add('row-cols-md-2');
 firstRowDiv.classList.add('row-cols-sm-1');
 shopWrapperDiv.appendChild(firstRowDiv);
 
-const product1Div = createProductDiv('headphones_product1', 'Headphones1', "https://media.istockphoto.com/id/1325906677/photo/modern-design-of-black-color-wireless-earphone-isolated.jpg?b=1&s=170667a&w=0&k=20&c=NqkzH2XxDe81TB2WzNIHDIMr_r7NCeD8y-kDRmgCg0M=",700);
-const product2Div = createProductDiv('headphones_product2', 'Headphones2',"https://cdn.pixabay.com/photo/2016/10/06/22/29/headphones-1720164__340.jpg",702);
-const product3Div = createProductDiv('headphones_product3', 'Headphones3', "https://media.istockphoto.com/id/1246138278/photo/silver-metallic-white-wireless-headphones-in-the-air-isolated-on-white-background-music.jpg?b=1&s=170667a&w=0&k=20&c=3tiFSD3oRoQXUoOVA6S2EommW5JUDciyUXBqjfSPyLQ=",700);
+const product1Div = createProductDiv('headphones_product1', 'Headphones1', "https://media.istockphoto.com/id/1325906677/photo/modern-design-of-black-color-wireless-earphone-isolated.jpg?b=1&s=170667a&w=0&k=20&c=NqkzH2XxDe81TB2WzNIHDIMr_r7NCeD8y-kDRmgCg0M=", 700);
+const product2Div = createProductDiv('headphones_product2', 'Headphones2', "https://cdn.pixabay.com/photo/2016/10/06/22/29/headphones-1720164__340.jpg", 702);
+const product3Div = createProductDiv('headphones_product3', 'Headphones3', "https://media.istockphoto.com/id/1246138278/photo/silver-metallic-white-wireless-headphones-in-the-air-isolated-on-white-background-music.jpg?b=1&s=170667a&w=0&k=20&c=3tiFSD3oRoQXUoOVA6S2EommW5JUDciyUXBqjfSPyLQ=", 700);
 
 
 firstRowDiv.appendChild(product1Div);
@@ -39,38 +39,31 @@ firstRowDiv.appendChild(product3Div);
 
 
 // Create the second row of products
-const secondRowDiv = document.createElement('div');
-secondRowDiv.classList.add('row');
-shopWrapperDiv.appendChild(secondRowDiv);
+// const secondRowDiv = document.createElement('div');
+// secondRowDiv.classList.add('row');
+// shopWrapperDiv.appendChild(secondRowDiv);
 
-const product4Div = createProductDiv('headphones_product4' ,'Headphones4',"https://media.istockphoto.com/id/860853774/photo/blue-headphones-isolated-on-a-white-background.jpg?b=1&s=170667a&w=0&k=20&c=gGPzyBznbjrCMb1tXjUi6js6Dh-vCp4XIgAaSERnIhs=",700);
-const product5Div = createProductDiv('headphones_product5', 'Headphones5',"https://cdn.pixabay.com/photo/2019/06/22/04/38/headphones-4290812__340.jpg",701);
-const product6Div = createProductDiv('headphones_product6', 'Headphones6',"https://media.istockphoto.com/id/1182209797/photo/close-view-of-wireless-black-gaming-headphones-with-microphone-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=YkT5jyY-NFh1Zhkhzvps_h226pLoPLie1CgpDw27SJA=",700);
+const product4Div = createProductDiv('headphones_product4', 'Headphones4', "https://media.istockphoto.com/id/860853774/photo/blue-headphones-isolated-on-a-white-background.jpg?b=1&s=170667a&w=0&k=20&c=gGPzyBznbjrCMb1tXjUi6js6Dh-vCp4XIgAaSERnIhs=", 700);
+const product5Div = createProductDiv('headphones_product5', 'Headphones5', "https://cdn.pixabay.com/photo/2019/06/22/04/38/headphones-4290812__340.jpg", 701);
+const product6Div = createProductDiv('headphones_product6', 'Headphones6', "https://media.istockphoto.com/id/1182209797/photo/close-view-of-wireless-black-gaming-headphones-with-microphone-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=YkT5jyY-NFh1Zhkhzvps_h226pLoPLie1CgpDw27SJA=", 700);
 
 firstRowDiv.appendChild(product4Div);
 firstRowDiv.appendChild(product5Div);
 firstRowDiv.appendChild(product6Div);
-
+let cartItems = [];
 // Function to create a product div
 function createProductDiv(imgAltText, productTitle, productImgLink, productprice) {
   const productDiv = document.createElement('div');
   productDiv.classList.add('d-flex');
   productDiv.classList.add('flex-sm-column');
   productDiv.classList.add('align-items-center');
-  
+
   const productImg = document.createElement('img');
   productImg.setAttribute('src', productImgLink);
   productImg.setAttribute('alt', imgAltText);
 
   productImg.classList.add('product-img');
   productDiv.append(productImg);
-  
-  //product title
-  // const productTitleP = document.createElement('span');
-  // productTitleP.textContent = '1500';
-  // productTitleP.classList.add("btn" ,"btn-outline-dark",'btn-cart');
-  // productDiv.appendChild(productTitleP);
-
 
   //price tag
   const price = document.createElement('span');
@@ -78,23 +71,50 @@ function createProductDiv(imgAltText, productTitle, productImgLink, productprice
   const strong = document.createElement('strong');
   strong.innerText = productprice;
   price.append(strong);
-  // productprice.classList.add('pricerate');
   productDiv.appendChild(price);
-
 
   const addToCartBtn = document.createElement('a');
   addToCartBtn.setAttribute('href', '#');
   addToCartBtn.classList.add('btn', 'btn-secondary', 'btn-cart');
- addToCartBtn.innerHTML=`<box-icon name='cart' color="white" size="cssSize" data-target="#model-cart"
- data-toggle="modal" id="cart-icon"></box-icon>`
+  addToCartBtn.innerHTML = `<box-icon name='cart' color="white" size="cssSize"></box-icon>`
+
+  addToCartBtn.addEventListener('click', () => {
+    // add item to cart
+    const item = {
+      imgAltText: imgAltText,
+      productTitle: productTitle,
+      productImgLink: productImgLink,
+      productprice: productprice
+    };
+    cartItems.push(item);
+
+    // update cart modal
+    const showCartTable = document.querySelector('.show-cart');
+    const totalCart = document.querySelector('.total-cart');
+    showCartTable.innerHTML = '';
+    let totalPrice = 0;
+    cartItems.forEach((item, index) => {
+      const cartRow = document.createElement('tr');
+      cartRow.dataset.index = index;
+      cartRow.innerHTML = `
+      <td><img src="${item.productImgLink}" alt="${item.imgAltText}" width="50"></td>
+      <td>${item.productTitle}</td>
+      <td>₹${item.productprice}</td>
+      <td><button class="btn btn-danger btn-sm remove-btn">Remove</button></td>
+    `;
+      showCartTable.appendChild(cartRow);
+      totalPrice += item.productprice;
+    });
+    totalCart.innerText = totalPrice;
+  });
+
+
+
   productDiv.appendChild(addToCartBtn);
-  
-
-
-
 
   return productDiv;
 }
+
 
 // Add the shop section to the document
 document.body.appendChild(shopSection);
